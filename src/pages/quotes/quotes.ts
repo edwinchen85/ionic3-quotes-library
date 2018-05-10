c587226b16fa2a4c5335968bfd534448aab5b198
Edwin Chen <edwinchen85@gmail.com>
2018-05-10 10:23:04 +0800

    Passing Quotes data between pages

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,7 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NavParams } from "ionic-angular";
+import { Quote } from "../../data/quote.interface";
 
 @Component({
   selector: "page-quotes",
   templateUrl: "quotes.html"
 })
-export class QuotesPage {}
+export class QuotesPage implements OnInit {
+  quoteGroup: { category: string; quotes: Quote[]; icon: string };
+
+  constructor(private navParams: NavParams) {}
+
+  ngOnInit() {
+    this.quoteGroup = this.navParams.data;
+  }
+
+  // ionViewDidLoad() {
+  //   this.quoteGroup = this.navParams.data;
+  // Add elvis operator (?) in template to use the approach
+  // }
+}
