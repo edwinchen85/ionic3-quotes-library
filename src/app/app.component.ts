82b2f17571b6075d3cc7d2f524c49ed6fa211458
Edwin Chen <edwinchen85@gmail.com>
2018-05-15 14:10:16 +0800

    Add a side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 
 import { TabsPage } from "../pages/tabs/tabs";
+import { SettingsPage } from "../pages/settings/settings";
 
 @Component({
   templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage: any = TabsPage;
+  tabsPage = TabsPage;
+  settingsPage = SettingsPage;
 
   constructor(
     platform: Platform,
@@ -23,4 +25,6 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  onLoad(page: any) {}
 }
