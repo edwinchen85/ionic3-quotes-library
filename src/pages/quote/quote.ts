2b0a2f77b3db50a6ca5689f8c4f801bfdc0d6b51
Edwin Chen <edwinchen85@gmail.com>
2018-05-15 10:04:30 +0800

    Dismiss modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,7 +1,14 @@
 import { Component } from "@angular/core";
+import { ViewController } from "ionic-angular";
 
 @Component({
   selector: "page-quote",
   templateUrl: "quote.html"
 })
-export class QuotePage {}
+export class QuotePage {
+  constructor(private viewCtrl: ViewController) {}
+
+  onClose() {
+    this.viewCtrl.dismiss();
+  }
+}
