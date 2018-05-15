6e49c5e0f28f7184db9d566ce592250e67b39950
Edwin Chen <edwinchen85@gmail.com>
2018-05-15 09:59:27 +0800

    Present quote page modal

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Quote } from "../../data/quote.interface";
 import { QuotesService } from "../../services/quotes";
+import { ModalController } from "ionic-angular";
+import { QuotePage } from "../quote/quote";
 
 @Component({
   selector: "page-favorites",
@@ -9,9 +11,17 @@ import { QuotesService } from "../../services/quotes";
 export class FavoritesPage {
   quotes: Quote[];
 
-  constructor(private quotesService: QuotesService) {}
+  constructor(
+    private quotesService: QuotesService,
+    private modalCtrl: ModalController
+  ) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
   }
 }
