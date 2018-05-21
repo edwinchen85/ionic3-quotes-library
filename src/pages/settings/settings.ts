d9e18f2363164d0bf2ac9b70b35973cc69245e5d
Edwin Chen <edwinchen85@gmail.com>
2018-05-21 12:00:38 +0800

    Prepare Settings Page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
+import { Toggle } from "ionic-angular";
 
 @Component({
   selector: "page-settings",
   templateUrl: "settings.html"
 })
-export class SettingsPage {}
+export class SettingsPage {
+  onToggle(toggle: Toggle) {
+    console.log(toggle);
+  }
+}
